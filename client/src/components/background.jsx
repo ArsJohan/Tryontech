f4fec9a77924a4c088c3194aacc36ef851571f3e
ArsJohan <133719384+ArsJohan@users.noreply.github.com>
2025-04-08 11:31:59 -0500

    Se agrego maquetación del background de la interfaz del login

---
+++ client/src/components/background.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import './background.css';
+
+export function Background(){
+    return (
+        <div className="bk-container">
+            <div className="bk-circle-blur"></div>
+
+        </div>
+
+
+    )
+
+
+}
+
+
+export default Background;
--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -5,37 +5,42 @@ import { Footer } from '../components/footer.jsx';
 import '../assets/styles/pages/login.css';
 import logout from '../assets/images/logout-icon.svg';
 import { BrowserRouter, UNSAFE_createBrowserHistory } from 'react-router-dom';
+import {Background} from '../components/background.jsx';
 
 export function Login() {
     return (
         
-        <Card>
-            <header className="lg-header">
-                <a href="login" className="logout">
-                    <img src={logout} alt="Logout" className="icon"/>
-                </a>
-            </header>
-            <div className="lg-title-container">
-                <div className="divider"></div>
-                <h1 className="lg-title">TryOnTech</h1>
-                <div className="divider"></div>
-            </div>
-            <div className="lg-form-container">
-                <h1 className="lg-form-title">Sign in</h1>
-                <label htmlFor="email" className="lg-form-lb">Email</label>
-                <input type="email" id="email" className="lg-form-username" placeholder="Example@gmail.com"/>
-                <label htmlFor="password" className="lg-form-lb">Password</label>
-                <input type="password" id="password" className="lg-form-password" placeholder="*****"/>
-                <a href="forgot" className="lg-form-forgot">Forgot your password?</a>
-                <div className="lg-form-btn-container">
-                    <BrowserRouter>
-                    <Button className={"bt-purple"} text={"Sign in" } to="/interfaz"></Button>
-                    <Button className={"bt-transparent"} text={"Sign up" } to="/interfaz"></Button>
-                    </BrowserRouter>
+        <div className='lg-container'>
+            <Background></Background>
+
+            <Card>
+                <header className="lg-header">
+                    <a href="login" className="logout">
+                        <img src={logout} alt="Logout" className="icon"/>
+                    </a>
+                </header>
+                <div className="lg-title-container">
+                    <div className="divider"></div>
+                    <h1 className="lg-title">TryOnTech</h1>
+                    <div className="divider"></div>
                 </div>
-            </div>
-            <Footer/>
-        </Card>
+                <div className="lg-form-container">
+                    <h1 className="lg-form-title">Sign in</h1>
+                    <label htmlFor="email" className="lg-form-lb">Email</label>
+                    <input type="email" id="email" className="lg-form-username" placeholder="Example@gmail.com"/>
+                    <label htmlFor="password" className="lg-form-lb">Password</label>
+                    <input type="password" id="password" className="lg-form-password" placeholder="*****"/>
+                    <a href="forgot" className="lg-form-forgot">Forgot your password?</a>
+                    <div className="lg-form-btn-container">
+                        <BrowserRouter>
+                        <Button className={"bt-purple"} text={"Sign in" } to="/interfaz"></Button>
+                        <Button className={"bt-transparent"} text={"Sign up" } to="/interfaz"></Button>
+                        </BrowserRouter>
+                    </div>
+                </div>
+                <Footer/>
+            </Card>
+        </div>
        
         
     )
