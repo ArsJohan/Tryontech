4c22ff7462e7723e9a0ca119761ca96c7702aeaf
Santiago <santigp0531@gmail.com>
2025-04-24 20:48:32 -0500

    Estructura base controlador login

--- TryontechWebAPI/Program.cs
+++ TryontechWebAPI/Program.cs
@@ -1,7 +1,27 @@
 using Microsoft.EntityFrameworkCore;
-//using TryontechWebAPI.Models;
+using TryontechWebAPI.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Configurar JWT Sgp
+var key = Encoding.UTF8.GetBytes("EstaEsUnaClaveSuperSeguraYMuyLarga1234567890"); // clave válida
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = "TryontechWebAPI",
+            ValidAudience = "TryontechWebAPIUsers",
+            IssuerSigningKey = new SymmetricSecurityKey(key)
+        };
+    });
 
 // Add services to the container.
 
@@ -13,6 +33,7 @@ builder.Services.AddControllers();
 
 var app = builder.Build();
 
+app.UseAuthentication(); // Habilitar autenticación Sgp
 // Configure the HTTP request pipeline.
 
 app.UseAuthorization();
