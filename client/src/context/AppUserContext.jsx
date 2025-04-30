fd3ccb88be78cea60710052817c5eee36b3bce99
ArsJohan <133719384+ArsJohan@users.noreply.github.com>
2025-04-30 11:02:49 -0500

    Ultimos Cambios

--- client/src/context/AppUserContext.jsx
+++ client/src/context/AppUserContext.jsx
@@ -8,8 +8,6 @@ export const AppProvider = ({ children }) => {
     const [selectedSex, setSelectedSex] = useState(""); // Sexo seleccionado
     const [IdCliente, setIdCliente] = useState(null); // ID del cliente
     const [imageUrl, setImageUrl] = useState(""); // URL de la imagen
-    const [bodyType, setBodyType] = useState(""); // Tipo de cuerpo
-    const [sexo, setSexo] = useState(""); // Sexo del usuario
 
     return (
         <AppContext.Provider
